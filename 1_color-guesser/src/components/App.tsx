8e24cf6e02471484e95690c5aaeb663c2257e19d
Samuel Lee <leesamuel423@gmail.com>
2023-05-05 02:33:06 -0400

    generate a random color on load

--- 1_color-guesser/src/components/App.tsx
+++ 1_color-guesser/src/components/App.tsx
@@ -1,11 +1,20 @@
+import React, { useState, useEffect } from 'react';
 import '../stylesheets/App.css'
 
 function App() {
+  const [color, setColor] = useState('') // useState to store the color
+
+  // useEffect to generate a random color (https://css-tricks.com/snippets/javascript/random-hex-color/)
+  useEffect(() => {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16)
+    setColor(`#${randomColor}`)
+  }, []);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div className="app">
+      <h1>Color Guesser</h1>
+      <div className="random-color" style={{ background: color }}></div>
+    </div>
   )
 }
 
